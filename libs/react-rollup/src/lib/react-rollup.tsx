6a4cd5803803230fc0a61c1b32a2b230749f06e2
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-01-04 21:02:17 +0200

    import in rollup lib

--- libs/react-rollup/src/lib/react-rollup.tsx
+++ libs/react-rollup/src/lib/react-rollup.tsx
@@ -1,4 +1,5 @@
 import styles from './react-rollup.module.css';
+import { utilsOne } from '@imported-libs/utils/one';
 
 /* eslint-disable-next-line */
 export interface ReactRollupProps {}
@@ -7,6 +8,7 @@ export function ReactRollup(props: ReactRollupProps) {
   return (
     <div className={styles['container']}>
       <h1>Welcome to ReactRollup!</h1>
+      <p>Here is the utils: {utilsOne()}</p>
     </div>
   );
 }
