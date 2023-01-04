aa90265f0122301a01290c238ed52bd7aa5f51f2
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-01-04 21:12:03 +0200

    added more importrs

--- apps/main-webpack/src/app/app.tsx
+++ apps/main-webpack/src/app/app.tsx
@@ -1,12 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 import { ReactRollup } from '@imported-libs/react-rollup';
+import { utilsOne } from '@imported-libs/utils/one';
 
 export function App() {
   return (
     <div className={styles['container']}>
       <ReactRollup />
       <h1>Welcome to MainWebpack!</h1>
+      <p>Here is the utils: {utilsOne()}</p>
     </div>
   );
 }
