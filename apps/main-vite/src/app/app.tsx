96905aa8165189374205bd0a882d463e6fbe4213
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-01-04 21:04:43 +0200

    import libs in aps

--- apps/main-vite/src/app/app.tsx
+++ apps/main-vite/src/app/app.tsx
@@ -1,13 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+import { ReactVite } from '@imported-libs/react-vite';
 
 export function App() {
   return (
-    <>
-      <NxWelcome title="main-vite" />
-      <div />
-    </>
+    <div className={styles['container']}>
+      <ReactVite />
+    </div>
   );
 }
 
--- apps/main-webpack/src/app/app.tsx
+++ apps/main-webpack/src/app/app.tsx
@@ -1,13 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+import { ReactRollup } from '@imported-libs/react-rollup';
 
 export function App() {
   return (
-    <>
-      <NxWelcome title="main-webpack" />
-      <div />
-    </>
+    <div className={styles['container']}>
+      <ReactRollup />
+      <h1>Welcome to MainWebpack!</h1>
+    </div>
   );
 }
 
